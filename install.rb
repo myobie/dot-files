60db6106a0b1f3137d756dd86903397b0696f903
myobie <nathan@myobie.com>
2012-11-09 12:53:08 -0800

    Install will now build .vim for me, so I will remove it

--- install.rb
+++ install.rb
@@ -1,16 +1,46 @@
+require 'pathname'
+
 def run_cmd(what)
   puts what
   `#{what}`
 end
 
-home = File.expand_path("~")
+# typing sucks
+def force?; ARGV.include?("force"); end
 
+def path(where); Pathname.new(where).realpath; end
+def mkdir(where); run_cmd "mkdir -p #{where}"; end
+def git(repo, where = nil); run_cmd("cd #{where || pwd} && git clone #{repo}"); end
+def update(where); run_cmd("cd #{where} && git reset --hard HEAD && git pull --rebase"); end
+def symlink(from, to)
+  opts = if force? then "sf" else "si" end
+  cmd = "ln -#{opts} #{from} #{to}"
+  run_cmd cmd
+end
+
+# paths
+this_file = Pathname.new(__FILE__)
+pwd = Pathname.pwd
+home = path(File.expand_path("~"))
+dot_vim = home.join(".vim")
+vundle = dot_vim.join("bundle/vundle")
+
+# symlink files
 Dir["{*,.*}"].each do |file|
-  unless File.directory?(file) || file == __FILE__
-    cmd = "ln -si #{Dir.pwd}/#{file} #{home}/#{File.basename(file)}"
-    run_cmd cmd
+  file = path(file)
+  unless file.directory? || file == this_file
+    symlink pwd.join(file), home.join(File.basename(file))
   end
 end
 
-vim_cmd = "ln -si #{Dir.pwd}/.vim #{home}/"
-run_cmd vim_cmd
+# .vim
+mkdir dot_vim.join("backup")
+mkdir dot_vim.join("bundle")
+
+unless vundle.exist?
+  git "git://github.com/gmarik/vundle.git", vundle
+end
+
+update vundle
+
+symlink pwd.join(".vim"), dot_vim
