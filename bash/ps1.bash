3554c0b7ce42550958f463301984499286b53dcd
myobie <me@nathanherald.com>
2017-01-12 10:22:05 +0100

    New ps1 that only uses ascii

--- bash/ps1.bash
+++ bash/ps1.bash
@@ -1,19 +1,19 @@
 function parse_git_dirty {
   if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]]; then
-    echo " ⌁"
+    echo " dirty"
   fi
 }
 function parse_git_needs_push {
   if [[ $(git status 2> /dev/null | grep 'Your branch is ahead') ]]; then
-    echo " ↗"
+    echo " ahead"
   fi
 }
 
 function number_of_background_jobs {
   number_of_jobs=$(jobs | wc -l | tr -d ' ')
   if [[ $number_of_jobs != "0" ]]; then
-    echo "⎌ $number_of_jobs "
+    echo "bg($number_of_jobs) "
   fi
 }
 
-PS1='→ `fancy_directory` $(__git_ps1 "⎇ %s$(parse_git_dirty)$(parse_git_needs_push) ")`number_of_background_jobs`\[\033[00;33m\]$\[\033[00m\] '; export PS1
+PS1='`fancy_directory` $(__git_ps1 "git(%s$(parse_git_dirty)$(parse_git_needs_push)) ")`number_of_background_jobs`\[\033[00;33m\]$\[\033[00m\] '; export PS1
