059bd89f4b02eb8f2f7afd1e3a348a96bc83bd66
Nathan Herald <me@nathanherald.com>
2021-12-28 14:09:28 +0100

    sed fix

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,13 +6,13 @@ repo=$1
 force=$2
 
 if [[ -z ${repo} || -z ${force} ]]; then
-  1>&2 echo "Incorrect arguments given to setup_vim.sh; repo: $repo force: $force"
+  echo 1>&2 "Incorrect arguments given to setup_vim.sh; repo: $repo force: $force"
   exit 1
 fi
 
-1>&2 echo "**"
-1>&2 echo "** Setup vim"
-1>&2 echo "**"
+echo 1>&2 "**"
+echo 1>&2 "** Setup vim"
+echo 1>&2 "**"
 
 if [[ -n $(which vim) ]]; then
   mkdir -p ~/.vim/backup
@@ -33,11 +33,11 @@ if [[ -n $(which vim) ]]; then
 
   if [[ ! -f ${spell_file} || ${force} == "1" ]]; then
     vim "+set spell" +spelldump +2,2d "+sav! /tmp/spelldump.txt" +qall
-    sed -i '' -e $'1s;^;/encoding=utf-8\\n;' /tmp/spelldump.txt
-    cat /tmp/spelldump.txt | sed "s/'/’/g" > $spell_file
+    sed -i -e $'1s;^;/encoding=utf-8\\n;' /tmp/spelldump.txt
+    cat /tmp/spelldump.txt | sed "s/'/’/g" >$spell_file
     vim "+mkspell! ${spell_file}" +qall
     rm /tmp/spelldump.txt
   fi
 else
-  1>&2 echo "No vim."
+  echo 1>&2 "No vim."
 fi
