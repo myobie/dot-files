f76aa4f725c1acdd9bce1c6e0786b290b4a676f4
Nathan Herald <me@nathanherald.com>
2022-02-15 16:25:08 +0100

    No ssh config, fully rely on Secretive

--- mac/bash/secretive.bash
+++ mac/bash/secretive.bash
@@ -1,5 +1,5 @@
 sock_path=${HOME}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 
-if [[ -f ${sock_path} ]]; then
+if [[ -r ${sock_path} ]]; then
   export SSH_AUTH_SOCK="${sock_path}"
 fi
