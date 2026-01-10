5456b2cf3b6781c7d93de5e96cec2da15855079f
Nathan Herald <me@nathanherald.com>
2026-01-10 11:07:36 +0100

    No more fancy title, remove biome, add zellij aliases

--- mac/bash/aliases.bash
+++ mac/bash/aliases.bash
@@ -1,3 +1,4 @@
 alias ls='ls -FG'
 alias mv='mv -nv'
 alias flushdns="dscacheutil -flushcache"
+alias rmdd='trash ~/Library/Developer/Xcode/DerivedData/*'
--- shared/bash/aliases.bash
+++ shared/bash/aliases.bash
@@ -1,3 +1,6 @@
 alias cls='printf "\033c"'
 alias ll='ls -lah'
 alias flush_deno_file_cache='rm -rf $HOME/Library/Caches/deno/gen/file'
+
+alias zj='zellij'
+alias zja='zellij attach'
