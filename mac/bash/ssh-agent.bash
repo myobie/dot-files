1f2dfd4ddd59431f85a606e8cb94e5fb24822d05
Nathan Herald <me@nathanherald.com>
2026-02-04 00:13:20 +0100

    Don't use biometrics over ssh

--- mac/bash/ssh-agent.bash
+++ mac/bash/ssh-agent.bash
@@ -1,7 +1,7 @@
 function use_1password_ssh_agent() {
   one_p_path="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 
-  if [[ -r ${one_p_path} ]]; then
+  if [[ -S "${one_p_path" ]]; then
     export SSH_AUTH_SOCK="${one_p_path}"
   fi
 }
@@ -9,4 +9,9 @@ function use_1password_ssh_agent() {
 # Only use 1Password agent if not in an SSH session (allow agent forwarding)
 if [ -z "$SSH_CONNECTION" ]; then
   use_1password_ssh_agent
+fi
+
+# Don't use biometrics over ssh
+if [ -n "$SSH_CONNECTION" ]; then
+  export OP_BIOMETRIC_UNLOCK_ENABLED=false
 fi
