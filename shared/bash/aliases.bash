251ce08d6f992e15a11bf8de0ce9316e49f444a1
Nathan Herald <me@nathanherald.com>
2026-03-09 18:55:18 +0100

    Use kitten ssh

--- shared/bash/aliases.bash
+++ shared/bash/aliases.bash
@@ -4,3 +4,7 @@ alias flush_deno_file_cache='rm -rf $HOME/Library/Caches/deno/gen/file'
 
 alias zj='zellij'
 alias zja='zellij attach'
+
+if [[ "$TERM" == "xterm-kitty" ]]; then
+  alias ssh='kitten ssh'
+fi
